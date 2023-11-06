bce4c25910b8b34a6d08e832df04917b13a02f25
João Victor Campos <ripjv1234@gmail.com>
2023-11-06 18:39:10 -0300

    requisito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const verifyManager = data.employees.some((manager) => manager.managers.includes(id));
+  return verifyManager;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const { employees } = data;
+
+  const verifyManagerEmployee = ((person) => person.managers.includes(managerId));
+  const returnedFullName = ((person) => `${person.firstName} ${person.lastName}`);
+  const returnedEmployees = employees
+    .filter(verifyManagerEmployee)
+    .map(returnedFullName);
+  return returnedEmployees;
 };
 
 module.exports = { isManager, getRelatedEmployees };
