9d6f0cd48e2815a40ed42f7da651625a2c1bd777
João Victor Campos <ripjv1234@gmail.com>
2023-11-06 17:21:27 -0300

    arruma-lint-2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  const animalVerify = data.species.find((animalName) => animalName.name === animal)
-  const ageVerify = animalVerify.residents.every((animalAge) => animalAge.age >= age)
-   return ageVerify;  
-  };
-  
+  const animalVerify = data.species.find((animalName) => animalName.name === animal);
+  const ageVerify = animalVerify.residents.every((animalAge) => animalAge.age >= age);
+  return ageVerify;
+};
+
 module.exports = getAnimalsOlderThan;
