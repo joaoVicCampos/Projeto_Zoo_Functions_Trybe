377a045c699e01de91aee15cee2489a61031d982
João Victor Campos <ripjv1234@gmail.com>
2023-11-06 18:00:00 -0300

    requisito 3 finalizado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
-};
+  if (!employeeName) {
+    return {};
+  }
 
+  const verifyName = data.employees.find(
+    (employee) => employee.firstName === employeeName,
+  );
+  const verifyLastName = data.employees.find(
+    (employee) => employee.lastName === employeeName,
+  );
+  if (verifyName) {
+    return verifyName;
+  }
+  return verifyLastName;
+};
+console.log(getEmployeeByName('Nelson'));
 module.exports = getEmployeeByName;
