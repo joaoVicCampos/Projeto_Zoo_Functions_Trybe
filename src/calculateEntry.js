6e85960f4183af2f2acdbc96c7d55d3e218d1125
João Victor Campos <ripjv1234@gmail.com>
2023-11-07 13:53:30 -0300

    requisito 8 finalizado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui!!!
+  const counted = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      counted.child += 1;
+    } else if (entrant.age < 50) {
+      counted.adult += 1;
+    } else {
+      counted.senior += 1;
+    }
+  });
+  return counted;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui!!!
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = data.prices;
+  const numberEntrants = Object.values(countEntrants(entrants));
+  let sumEntrants = 0;
+  sumEntrants += numberEntrants[0] * child;
+  sumEntrants += numberEntrants[1] * adult;
+  sumEntrants += numberEntrants[2] * senior;
+  return sumEntrants;
 };
 
 module.exports = { calculateEntry, countEntrants };
