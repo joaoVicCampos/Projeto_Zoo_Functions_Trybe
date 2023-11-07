b8b11a8951c44c670b8593557cc0ac409d43c6a4
João Victor Campos <ripjv1234@gmail.com>
2023-11-06 23:13:41 -0300

    requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const { species } = data;
+
+  if (!animal) {
+    const animalCounter = {};
+    species.forEach((specie) => {
+      animalCounter[specie.name] = specie.residents.length;
+    });
+    return animalCounter;
+  }
+
+  const speciesName = animal.species;
+  const animalsCounted = species.find((specie) => specie.name === speciesName);
+
+  if (animal.sex) {
+    const sexCounted = animalsCounted.residents
+      .filter((resident) => resident.sex === animal.sex).length;
+    return sexCounted;
+  }
+
+  return animalsCounted.residents.length;
 };
+
+console.log(countAnimals({ species: 'lions' }));
 
 module.exports = countAnimals;
