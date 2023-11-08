a1498be4b1405f0995bb85811484f9d3719189d7
João Victor Campos <ripjv1234@gmail.com>
2023-11-08 14:23:11 -0300

    requisito 10 finalizado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,24 +1,19 @@
 const data = require('../data/zoo_data');
 
-const getOldestFromFirstSpecies = (id) => {
-  const responsibleEmployee = data.employees.find((employee) => employee.id === id);
-
-  if (!responsibleEmployee || !responsibleEmployee.responsibleFor.length) {
-    return null;
-  }
-
-  const [firstSpeciesId] = responsibleEmployee.responsibleFor;
-  const firstSpecies = data.species.find((specie) => specie.id === firstSpeciesId);
-
-  if (!firstSpecies || firstSpecies.residents.length === 0) {
-    return null;
-  }
-
-  const oldestAnimal = firstSpecies.residents.reduce((oldest, current) =>
-    current.age > oldest.age ? current : oldest
-  );
-
-  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+const findEmployeeById = (id) => data.employees.find((employee) => employee.id === id);
+const findFirstSpeciesForEmployee = (employee) => employee.responsibleFor[0];
+const findOldestAnimalForEmployee = (employee) => {
+  const speciesId = findFirstSpeciesForEmployee(employee);
+  const species = data.species.find((animal) => animal.id === speciesId);
+  const sortedResidents = species.residents.sort((a, b) => b.age - a.age);
+  const residentResult = sortedResidents[0];
+  return [residentResult.name, residentResult.sex, residentResult.age];
 };
-
+const getOldestFromFirstSpecies = (id) => {
+  const employee = findEmployeeById(id);
+  if (!employee) {
+    return 'Funcionário não encontrado.';
+  }
+  return findOldestAnimalForEmployee(employee);
+};
 module.exports = getOldestFromFirstSpecies;
